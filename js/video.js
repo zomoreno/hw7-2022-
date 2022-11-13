74e3a61808e52f98d38829ef0815bd7e8cb548d4
Zochi Moreno-Martinez <112501810+zomoreno@users.noreply.github.com>
2022-11-12 20:11:18 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -19,9 +19,58 @@ document.querySelector("#pause").addEventListener("click", function() {
 });
 document.querySelector("#slower").addEventListener("click", function() {
 	console.log("Slow down Video")
-	video.playbackRate = 0.5;
+	var video = document.querySelector("#player1")
+	video.playbackRate *= .9;
+	console.log("video speed is " + videolplaybackRate)
 });
 document.querySelector("#faster").addEventListener("click", function() {
-	console.log("Speed up Video")
-	video.playbackRate = 2;
+	console.log("Speed Up Video")
+	var video = document.querySelector("#player1")
+	video.playbackRate /= .9;
+	console.log("video speed is " + videolplaybackRate)
 });
+
+document.querySelector("#skip").addEventListener("click", function() {
+	console.log("Skip to Video")
+	var video = document.querySelector("#player1");
+	video.currentTime += 10;
+	console.log("current time is " + video.currentTime)
+	video.onended = function() {
+		alert("The video has ended");
+		video.currentTime = 0;
+	}
+});
+document.querySelector("#mute").addEventListener("click", function() {
+	console.log("Mute Video")
+	if(video.muted == true){
+		console.log("Unmute")
+		video.muted = false
+		this.innerHTML = "Mute"
+	}
+	else{
+		console.log("Mute")
+		video.muted = true
+		this.innerHTML = "Unmute"
+	}
+});
+
+document.querySelector("#slider").addEventListener("click", function(){
+	console.log("The current value is ", this.value)
+	video.volume = this.value/100
+	document.querySelector("#volume").innerHTML = video.volume * 100 + "%"
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add("oldSchool")
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	video.classList.remove("oldSchool")
+});
+// document.querySelector("#skip").addEventListener("click", function() {
+// 	console.log("Skip to Video")
+// 	var video = document.getElementById("myVideo");
+// 	video.onended = function() {
+//     alert("The video has ended");
+// 	}
+// });
